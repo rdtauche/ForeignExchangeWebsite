3151d3fe7102dec19ce2452a98a098516e4a93d0
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-03-28 21:58:03 -0400

    got the streaming rates api to work

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,23 @@
 var EURUSD = document.querySelector("#EURUSD");
+var GBPUSD = document.querySelector("#GBPUSD");
+var USDCAD = document.querySelector("#USDCAD");
+var USDCHF = document.querySelector("#USDCHF");
+var USDNOK = document.querySelector("#USDNOK");
+var USDSEK = document.querySelector("#USDSEK");
+var USDJPY = document.querySelector("#USDJPY");
+var AUDUSD = document.querySelector("#AUDUSD");
+var NZDUSD = document.querySelector("#NZDUSD");
+var USDBRL = document.querySelector("#USDBRL");
+var USDMXN = document.querySelector("#USDMXN");
+var USDPHP = document.querySelector("#USDPHP");
+var USDARS = document.querySelector("#USDARS");
+var USDPEN = document.querySelector("#USDPEN");
+var USDCRC = document.querySelector("#USDCRC");
+var USDUYU = document.querySelector("#USDUYU");
+var USDSAR = document.querySelector("#USDSAR");
+var USDSGD = document.querySelector("#USDSGD");
+var USDJPY = document.querySelector("#USDJPY");
+
 let url = 'https://api.exchangeratesapi.io/v1';
 
 function params(paramsObj) {
@@ -18,7 +37,28 @@ function getLatest(options) {
 
     function displayData(rates) {
         console.log(rates)
-            EURUSD.innerText=`${rates.EUR}`;
+        // console.log(rates.rates)
+        // console.log(rates.rates.EUR)
+            EURUSD.innerText=`${"EUR/USD " + rates.rates.EUR}`;
+            GBPUSD.innerText=`${"GBP/USD " + rates.rates.GBP}`;
+            USDCAD.innerText=`${"USD/CAD " + rates.rates.CAD}`;
+            USDCHF.innerText=`${"USD/CHF " + rates.rates.CHF}`;
+            USDNOK.innerText=`${"USD/NOK " + rates.rates.NOK}`;
+            USDSEK.innerText=`${"USD/SEK " + rates.rates.SEK}`;
+            USDJPY.innerText=`${"USD/JPY " + rates.rates.JPY}`;
+            AUDUSD.innerText=`${"AUD/USD " + rates.rates.AUD}`;
+            NZDUSD.innerText=`${"NZD/USD " + rates.rates.NZD}`;
+            USDBRL.innerText=`${"USD/BRL " + rates.rates.BRL}`;
+            USDMXN.innerText=`${"USD/MXN " + rates.rates.MXN}`;
+            USDPHP.innerText=`${"USD/PHP " + rates.rates.PHP}`;
+            USDARS.innerText=`${"USD/ARS " + rates.rates.ARS}`;
+            USDPEN.innerText=`${"USD/PEN " + rates.rates.PEN}`;
+            USDCRC.innerText=`${"USD/CRC " + rates.rates.CRC}`;
+            USDUYU.innerText=`${"USD/UYU " + rates.rates.UYU}`;
+            USDSAR.innerText=`${"USD/SAR " + rates.rates.SAR}`;
+            USDSGD.innerText=`${"USD/SGD " + rates.rates.SGD}`;
+            USDJPY.innerText=`${"USD/JPY " + rates.rates.JPY}`;
+
     }
 };
 
